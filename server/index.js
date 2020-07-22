c42555d7ed6987a1793a1bee0ad855b4e90df446
Mann Mehta <17it051@charusat.edu.in>
2020-07-22 20:28:34 +0530

    Added react frontend

--- server/index.js
+++ server/index.js
@@ -1,31 +1,35 @@
 const express = require("express");
 const app = express();
-const cors = require("cors");
-const poll = require("./db");
+const pool = require("./db");
 
 //minddleware
-app.use(cors());
 app.use(express.json());
 
 app.listen(5000, () => {
     console.log("Server started on port 5000");
 });
-
 // ROUTES
 
-/// create user data in a TABLE
+/// Post
 app.post("/user", async(req,res) => {
     try {
-        console.log(req.body);      
+        const {fname, lname, phno, email, dob, pwd, gender, languages, dateadded} = req.body;
+        var query = "SELECT * from user_data WHERE email = $1";
+        const checkValidity = await pool.query(query,[email]);
+        
+        if(checkValidity.rowCount!=0) {
+            err = new Error();
+            err.message = "USER ALREADY EXIST WITH THIS EMAIL";
+            throw err;
+        }
+        else {
+            const newUser = await pool.query("INSERT INTO user_data (fname,lname,phno,email,dob,pwd,gender,languages,dateadded) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9) RETURNING *" ,
+            [fname, lname, phno, email, dob, pwd, gender, languages, dateadded]);
+            res.statusCode = 200;
+            res.json(newUser.rows[0]);          
+        }
+        
     } catch(err) {
-        console.log(err.message);
-    }
-})
-
-app.get("/user", async(req,res) => {
-    try {
-        console.log(req.body);      
-    } catch(err) {
-        console.log(err.message);
+        res.json(err);
     }
 });
