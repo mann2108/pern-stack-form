eb414e2da0799e14d02ab33e43b11a6ea0cb3d33
Mann Mehta <17it051@charusat.edu.in>
2020-07-22 14:40:40 +0530

    Inital Commit

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+//minddleware
+app.use(cors());
+app.use(express.json());
+
+app.listen(5000, () => {
+    console.log("Server started on port 5000");
+});
